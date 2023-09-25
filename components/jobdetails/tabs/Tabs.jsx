321eb9bcc3223790a2ba79e49dd2767fdaf6a5fa
GideonOdiokine <gidysmartodiokine@gmail.com>
2023-09-25 16:30:05 +0100

    added JobDetails page

--- components/jobdetails/tabs/Tabs.jsx
+++ components/jobdetails/tabs/Tabs.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, FlatList } from 'react-native'
 
 import styles from './tabs.style'
+import { SIZES } from '../../../constants'
 
-const Tabs = () => {
+const TabButton = ({name, activeTab, onHandleSearchType})=>(
+ <TouchableOpacity onPress={onHandleSearchType} >
+    <Text>{name}</Text>
+ </TouchableOpacity>
+)
+
+const Tabs = ({tabs, setActive, activeTab}) => {
   return (
-    <View>
-      <Text>Tabs</Text>
+    <View style={styles.container}>
+     <FlatList data={tabs}
+     renderItem={({item})=>(
+        <TabButton name={item} activeTab={activeTab} onHandleSearchType={()=> setActive(item)} />
+     )}
+     />
     </View>
   )
 }
--- screens/job-details/JobDetails.tsx
+++ screens/job-details/JobDetails.tsx
@@ -20,6 +20,8 @@ import {useRoute} from '@react-navigation/native';
 import {useFetch} from '../../hook/useFetch';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
+const tabs = ['About', 'Qualifications', 'Responsibilities'];
+
 const JobDetails = () => {
   const route = useRoute();
   const {id}: any = route?.params;
@@ -30,25 +32,39 @@ const JobDetails = () => {
   });
 
   const [refreshing, setRefreshing] = useState(false);
+  const [activeTab, setActiveTab] = useState(tabs[0]);
   const onRefresh = useCallback(() => {}, []);
 
   return (
     <SafeAreaView style={{flex: 1, backgroundColor: COLORS.lightWhite}}>
       <>
-        <ScrollView showsVerticalScrollIndicator={false} refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} /> } >
-        {loading ? (
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+          }>
+          {loading ? (
             <ActivityIndicator size="large" color={COLORS.primary} />
-        ): error?(
-            <Text>{"Something went wrong"}</Text>
-        ): data.length === 0 ? (
-            <Text>{"No data found"}</Text>
-        ):(
+          ) : error ? (
+            <Text>{'Something went wrong'}</Text>
+          ) : data.length === 0 ? (
+            <Text>{'No data found'}</Text>
+          ) : (
             <View style={{flex: 1, padding: SIZES.medium}}>
-             <Company  companyLogo={data[0].employer_logo}
-             jobTitle={data[0].job_title} companyName={data[0].employer_name} location={data[0].job_country} />
-             {/* <JobAbout data={data} /> */}
+              <Company
+                companyLogo={data[0].employer_logo}
+                jobTitle={data[0].job_title}
+                companyName={data[0].employer_name}
+                location={data[0].job_country}
+              />
+              {/* <JobAbout data={data} /> */}
+              <JobTabs
+                tabs={tabs}
+                activeTab={activeTab}
+                setActiveTab={setActiveTab}
+              />
             </View>
-        )}
+          )}
         </ScrollView>
       </>
     </SafeAreaView>
